39fbaa168b9cdcf3af90db170ddbf88806c84152
Besufekad-HAZ <besurye@gmail.com>
2024-09-06 13:10:24 +0300

    Update the dont initialize the configs and Create Header component to work on

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,13 @@ import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
+// Components
+import Header from "@/components/Header";
+
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
-  weight: ["100", "200","300", "400", "500", "600", "700", "800" ],
-  variable: '--font-jetbrainsMono',
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-jetbrainsMono",
 });
 
 export const metadata: Metadata = {
@@ -20,9 +23,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={jetbrainsMono.variable}
-      >
+      <body className={jetbrainsMono.variable}>
+        <Header />
         {children}
       </body>
     </html>
