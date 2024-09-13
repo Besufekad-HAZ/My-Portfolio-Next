96aed76beecf6eb49d78b3f98ee12b78e7978cb2
Besufekad-HAZ <besurye@gmail.com>
2024-09-13 13:17:47 +0300

    Structure the Mobile and desktop nav and start working on the overall Nav component page

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,20 +7,23 @@ import Nav from "./Nav";
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mxauto">
+      <div className="container mx-auto flex justify-between items-center">
         {/* logo */}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
-            Besu <span className="text-accent">.</span>
+            Besu<span className="text-accent">.</span>
           </h1>
         </Link>
         {/* desktop nav & hire me button */}
-        <div className="hidden xl:flex">
+        <div className="hidden xl:flex items-center gap-8">
           <Nav />
           <Link href="/contact">
             <Button>Hire me.</Button>
           </Link>
         </div>
+
+        {/* mobile nav */}
+        <div className="xl:hidden">Mobile Nav</div>
       </div>
     </header>
   );
