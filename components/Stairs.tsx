bd9f2129ff09cc97391937e78f8ac2de1b8cf7f9
Besufekad-HAZ <besurye@gmail.com>
2024-09-13 14:26:52 +0300

    Initialize the Stairs component for animation

--- components/Stairs.tsx
+++ components/Stairs.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { motion } from "framer-motion";
+
+const stairAnimation = {
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
 
 const Stairs = () => {
-  return (
-    <div>Stairs</div>
-  )
-}
+  return <div>Stairs</div>;
+};
 
-export default Stairs
+export default Stairs;
