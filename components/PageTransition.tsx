e0fa4278dc6f1e8a0d0cd9d7959a98d5f6e9462d
Besufekad-HAZ <besurye@gmail.com>
2024-09-13 14:04:47 +0300

    Install framer motion package and work on the page transition component

---
+++ components/PageTransition.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+const PageTransition = ({ children }) => {
+  return <AnimatePresence>{children}</AnimatePresence>;
+};
+
+export default PageTransition;
