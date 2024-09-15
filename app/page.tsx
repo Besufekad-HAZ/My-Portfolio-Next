c6281f7feb5f9d7821d92e6e861c070ad4ab468c
Besufekad-HAZ <besurye@gmail.com>
2024-09-15 21:33:11 +0300

    Remove Unused declaration from mobile nav component and add button and react icons to the Homepage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
 
 const Home = () => {
   return <div>Hompage</div>;
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -3,7 +3,6 @@
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { CiMenuFries } from "react-icons/ci";
 
 const links = [
