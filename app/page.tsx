871246922c225bec723ae1574982257fd8220797
Besufekad-HAZ <besurye@gmail.com>
2024-09-15 22:24:33 +0300

    Style and align the hompage text and image space and leave space for button and socials

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,32 @@ import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
 const Home = () => {
-  return <div>Hompage</div>;
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div
+          className="flex flex-col xl:flex-row items-center justify-between
+          xl:pt-8 xl:pb-24"
+        >
+          {/* text  */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Fullstack Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br />
+              <span className="text-accent">Besufekad Alemu</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I specialize in creating seamless digital experiences and possess
+              expertise in a wide range of programming languages and
+              technologies.
+            </p>
+          </div>
+          {/* photo */}
+          <div>Photo</div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
