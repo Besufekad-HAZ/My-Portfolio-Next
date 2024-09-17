3a46b26b6502633fb6dacdcdc6a772d91e7daa73
Besufekad-HAZ <besurye@gmail.com>
2024-09-18 00:03:18 +0300

    Update the my services array inside the services page component page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -8,6 +8,25 @@ const services = [
     num: "01",
     title: "Web Development",
     text: "I develop and design websites with a focus on performance and user experience. I am well versed in HTML, CSS, and JavaScript. I also have experience with React and Next.js.",
+    href: "/web-development",
+  },
+  {
+    num: "02",
+    title: "Mobile App Development",
+    text: "I develop and design mobile apps with a focus on performance and user experience. I am well versed in HTML, CSS, and JavaScript. I also have experience with React and Next.js.",
+    href: "/mobile-development",
+  },
+  {
+    num: "03",
+    title: "Game Development",
+    text: "I develop and design games with a focus on performance and user experience. I am well versed in HTML, CSS, and JavaScript. I also have experience with React and Next.js.",
+    href: "/game-development",
+  },
+  {
+    num: "04",
+    title: "AI Development",
+    text: "I develop and design AI with a focus on performance and user experience. I am well versed in HTML, CSS, and JavaScript. I also have experience with React and Next.js.",
+    href: "/ai-development",
   },
 ];
 
