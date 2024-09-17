5f41461009f39db9e458e968e09f1c4e4c719067
Besufekad-HAZ <besurye@gmail.com>
2024-09-18 00:00:43 +0300

    Start Workin on the services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,4 +1,15 @@
-import React from "react";
+"use client";
+
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    text: "I develop and design websites with a focus on performance and user experience. I am well versed in HTML, CSS, and JavaScript. I also have experience with React and Next.js.",
+  },
+];
 
 const Services = () => {
   return <div>Services page</div>;
