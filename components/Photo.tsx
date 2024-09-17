d835b49386d28a5ec721852a0f39ad583f40f20c
Besufekad-HAZ <besurye@gmail.com>
2024-09-17 23:38:22 +0300

    Fix some layout and styling errors in the Photo component page

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -24,7 +24,7 @@ const Photo = () => {
           absolute"
         >
           <Image
-            src="/assets/photo.png"
+            src="/assets/photo1.png"
             priority
             quality={100}
             fill
@@ -35,7 +35,7 @@ const Photo = () => {
 
         {/* circle */}
         <motion.svg
-          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px"
+          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
           fill="transparent"
           viewBox="0 0 506 506"
           xmlns="http://www.w3.org/2000/svg"
--- components/Stats.tsx
+++ components/Stats.tsx
@@ -1,4 +1,25 @@
 "use client";
+
+import CountUp from "react-countup";
+
+const stats = [
+  {
+    num: 6,
+    text: "Total Years of Experience",
+  },
+  {
+    num: 50,
+    text: "Projects completed",
+  },
+  {
+    num: 12,
+    text: "Technologies Mastered",
+  },
+  {
+    num: 1800,
+    text: "Code Commits",
+  },
+];
 const Stats = () => {
   return <div>Stats</div>;
 };
