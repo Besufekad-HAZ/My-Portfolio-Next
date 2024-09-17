c489ed265c13a6442be2d9feb9c214ab16dee8b3
Besufekad-HAZ <besurye@gmail.com>
2024-09-17 23:46:48 +0300

    Work on the stats page to display item.text

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -30,13 +30,18 @@ const Stats = () => {
         >
           {stats.map((item, index) => {
             return (
-              <div className="flex-1 flex gap-4 items-center" key={index}>
+              <div
+                className="flex-1 flex gap-4 items-center
+                justify-center xl:justify-start"
+                key={index}
+              >
                 <CountUp
                   end={item.num}
                   duration={5}
                   delay={2}
                   className="text-4xl xl:text-6xl font-extrabold"
                 />
+                <p>{item.text}</p>
               </div>
             );
           })}
