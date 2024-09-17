664dad46d40771aa8d5f5abd7968ead3dd6b72da
Besufekad-HAZ <besurye@gmail.com>
2024-09-17 23:04:03 +0300

    Style the circle in the Photo component

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -33,16 +33,27 @@ const Photo = () => {
         </motion.div>
 
         {/* circle */}
-       <motion.svg>
-        <motion.circle
-          cx="149"
-          cy="149"
-          r="149"
-          fill="none"
-          strokeWidth="1"
-          stroke="white"
-        />
-       </motion.svg>
+        <motion.svg
+          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            strokeWidth="4"
+            stroke="#00ff99"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{strokeDasharray: "24 10 0 0"}}
+            animate={{
+                strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+                rotate: [120, 360]
+            }}
+          />
+        </motion.svg>
       </motion.div>
     </div>
   );
