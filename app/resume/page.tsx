01332a1d6200dff5be07ba0f01e6c811976c6fbb
Besufekad-HAZ <besurye@gmail.com>
2024-09-18 13:51:25 +0300

    work on the info section data of the resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -18,7 +18,38 @@ import {
 
 const about = {
   title: "About me",
-  Description: " I'm a Fullstack developer ",
+  Description:
+    "I'm a Fullstack developer with a passion for building dynamic and responsive web applications. ",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Besufekad Alemu",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+251) 923027215",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "5+ years",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Ethiopian",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "Besurye@gmail.com",
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available",
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "Amharic, English",
+    },
+  ],
 };
 const Resume = () => {
   return <div>Resume Page</div>;
