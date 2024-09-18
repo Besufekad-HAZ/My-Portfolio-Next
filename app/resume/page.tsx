a127f126c9ccadbd290af3a436d6be7992e4624f
Besufekad-HAZ <besurye@gmail.com>
2024-09-18 14:37:18 +0300

    Work on the motion.div(framer motion) tablist and tabtriggers

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -181,7 +181,36 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { motion } from "framer-motion";
 
 const Resume = () => {
-  return <div>Resume Page</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12
+      xl:py-0"
+    >
+      <div className="conainer mx-auto">
+        <Tabs
+          defaultValue="experience"
+          className="flex flex-col xl:flex-row
+        gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto
+          xl:mx-0 gap-6">
+            <TabsTrigger value={""}>Experience</TabsTrigger>
+            <TabsTrigger value={""}>Education</TabsTrigger>
+            <TabsTrigger value={""}>Skills</TabsTrigger>
+            <TabsTrigger value={""}>About me</TabsTrigger>
+          </TabsList>
+
+          {/* content  */}
+          <div>content</div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Resume;
