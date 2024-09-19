3090b4d68239eafadaee04603aa4f186b2b829d4
Besufekad-HAZ <besurye@gmail.com>
2024-09-19 22:22:36 +0300

    UpdateTabsPrimitive.Content styling with min-h-[480px]

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -43,7 +43,7 @@ const TabsContent = React.forwardRef<
   <TabsPrimitive.Content
     ref={ref}
     className={cn(
-      "mt-2 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+      "min-h-[480px] ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
       className
     )}
     {...props}
