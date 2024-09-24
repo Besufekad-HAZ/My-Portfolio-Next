6a0329fdae6e95f5018586b91ab8054a9facc0c7
Besufekad-HAZ <besurye@gmail.com>
2024-09-24 13:24:54 +0300

    Update the photo and motion.svg circle size

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -20,7 +20,7 @@ const Photo = () => {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
           }}
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten
+          className="w-[298px] h-[298px] xl:w-[450px] xl:h-[450px] mix-blend-lighten
           absolute"
         >
           <Image
@@ -35,7 +35,7 @@ const Photo = () => {
 
         {/* circle */}
         <motion.svg
-          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+          className="w-[300px] xl:w-[450px] h-[300px] xl:h-[450px]"
           fill="transparent"
           viewBox="0 0 506 506"
           xmlns="http://www.w3.org/2000/svg"
