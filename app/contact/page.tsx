ba35e0d35a31924ec75ea13e8198fc0e9027c294
Besufekad-HAZ <besurye@gmail.com>
2024-09-24 22:11:00 +0300

    Import all the necessary packages in the contact paege

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,21 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"; // Importing from the UI component
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
+
 
 const Contact = () => {
   return <div>Contact Page</div>;
