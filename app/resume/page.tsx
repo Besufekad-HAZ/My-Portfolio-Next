0c6984efbec1d079aa7c119749f5c57b375eef41
Besufekad-HAZ <besurye@gmail.com>
2024-09-24 09:33:17 +0300

    Add scroll area with grid properties

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -213,7 +213,25 @@ const Resume = () => {
             <TabsContent value="experience" className="w-full ">
               <div className="flex flex-col gap-[30px] text-center xl:text-left">
                 <h3 className="text-4xl font-bold">{experience.title}</h3>
-                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{experience.description}</p>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item, index) => {
+                      return (
+                        <li>
+                          <span>{item.duration}</span>
+                          <h3>{item.position}</h3>
+                          <div>
+                            {/* dot  */}
+                            <span></span>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
               </div>
             </TabsContent>
             {/* education */}
