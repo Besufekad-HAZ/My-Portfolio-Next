76a62814491d6db6595bff76c4448536f22b6c39
Besufekad-HAZ <besurye@gmail.com>
2024-09-24 21:35:44 +0300

    Implement the PiCaret icons in Worksliderbtns component

--- components/ui/WorkSliderBtns.tsx
+++ components/ui/WorkSliderBtns.tsx
@@ -3,8 +3,18 @@
 import { useSwiper } from "swiper/react";
 import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
 
-const WorkSliderBtns = () => {
-  return <div>WorkSliderBtns</div>;
+const WorkSliderBtns = ({ containerStyles, btnStyles, iconsStyles }) => {
+  const swiper = useSwiper();
+  return (
+    <div className={containerStyles}>
+      <button className={btnStyles}>
+        <PiCaretLeftBold className={iconsStyles} />
+      </button>
+      <button className={btnStyles}>
+        <PiCaretRightBold className={iconsStyles} />
+      </button>
+    </div>
+  );
 };
 
 export default WorkSliderBtns;
