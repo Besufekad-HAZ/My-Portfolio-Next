3cac1a99e69cc4dba64aedf58114f451c638af8e
Besufekad-HAZ <besurye@gmail.com>
2024-09-24 13:45:16 +0300

    Import swiper and swiper styles to use it on the works page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -2,6 +2,13 @@
 
 // import framer motion
 import { motion } from "framer-motion";
+import { useState } from "react";
+
+// import swiper react
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import swiper styles
+import "swiper/css";
 
 const Work = () => {
   return <div>Work Page</div>;
