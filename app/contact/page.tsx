eb6023c66e337de80385bf05e08b6f11ea3908dd
Besufekad-HAZ <besurye@gmail.com>
2024-09-25 16:30:15 +0300

    Start working on the form and fill in the neccesary data inside the contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -46,34 +46,29 @@ const Contact = () => {
       className="py-6"
     >
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
-          <div>
-            <form
-              className="w-full flex flex-col gap-4
-          lg:flex-row"
-            >
-              <Input name="name" placeholder="Name" />
-              <Input name="email" type="email" placeholder="Email" />
-              <Input name="subject" placeholder="Subject" />
-              <Textarea name="message" placeholder="Message" />
-              <Button type="submit">Send</Button>
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <p className="text-white/60">
+                I'm excited to collaborate on your next project. Whether you
+                need a full-stack developer, a graphic designer, or IT support,
+                I'm here to help bring your vision to life. Let's create
+                something amazing together!
+              </p>
+              {/* input  */}
+              <div>
+                <Input />
+              </div>
             </form>
           </div>
           {/* info */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8">
-            {info.map((item, index) => (
-              <div
-                key={index}
-                className="flex items-center gap-4 bg-white/5 p-4 rounded-lg"
-              >
-                {item.icon}
-                <div>
-                  <h3 className="text-xl font-bold text-white">{item.title}</h3>
-                  <p className="text-white/60">{item.content}</p>
-                </div>
-              </div>
-            ))}
+          <div
+            className="flex-1 flex items-center xl:justify-end
+          order-1 xl:order-none mb-8 xl:mb-0"
+          >
+            items
           </div>
         </div>
       </div>
