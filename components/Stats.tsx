73e849ad0506aec50818ef662ce6de5651496bcb
Besufekad-HAZ <besurye@gmail.com>
2026-02-04 11:26:02 +0300

    Fixed invalid className syntax and fix CountUp usage issues

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -8,11 +8,11 @@ const stats = [
     text: "Total Years of Experience",
   },
   {
-    num: 60,
+    num: 80,
     text: "Projects completed",
   },
   {
-    num: 15,
+    num: 20,
     text: "Technologies Mastered",
   },
   {
@@ -24,23 +24,16 @@ const Stats = () => {
   return (
     <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
       <div className="container mx-auto">
-        <div
-          className="flex flex-wrap gap-6 max-w-[80vw] mx-auto
-        xl:max-w-none"
-        >
+        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
           {stats.map((item, index) => {
             return (
               <div
-                className="flex-1 flex gap-4 items-center
-                justify-center xl:justify-start"
+                className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
                 key={index}
               >
-                <CountUp
-                  end={item.num}
-                  duration={5}
-                  delay={2}
-                  className="text-4xl xl:text-6xl font-extrabold"
-                />
+                <span className="text-4xl xl:text-6xl font-extrabold">
+                  <CountUp end={item.num} duration={5} delay={2} />
+                </span>
                 <p
                   className={`${
                     item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
